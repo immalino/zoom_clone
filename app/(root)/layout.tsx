2ce2ce21591d2f3bc7483436ce48fe46443d00c6
Faza Dandy <fazadandyakmalin0@gmail.com>
2024-08-10 17:10:41 +0700

    setup create meeeting with "stream"

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,10 @@
+import StreamVideoProvider from "@/providers/StreamClientProviders";
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  return <main>{children}</main>;
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
 };
 export default RootLayout;
